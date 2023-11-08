dda7699bccd73f3cd86c7cf9200096bbf340ad74
syprogrammer <sonaly647@gmail.com>
2023-11-08 11:00:38 +0530

    maked search bar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,21 +10,21 @@ export default function Navbar() {
   
   return (
     <div className="nav flex justify-between  md:justify-evenly items-center shadow-md p-1 fixed bg-white  w-full h-[60px] z-10">
-      <div className="logo flex flex-col items-start">
-        <Link to="/" className="flex flex-col items-end md:px-4">
+      <div className="logo flex flex-col  items-start">
+        <Link to="/" className="flex flex-col items-end  md:px-4">
           <div className="w-fit">
-            <img src="./logo.svg" data-testid="logo" className="w-28" 
+            <img src="./logo.svg" data-testid="logo" className="w-20 md:w-28" 
             
             />
           </div>
           <div className="flex gap-1 items-center">
-            <span className="text-gray-700 italic text-xs">Explore</span>
-            <span className="text-yellow-500 italic  text-xs">Plus</span>
-            <img src="./fplus.svg" className="w-2" />
+            <span className="text-gray-700 italic text-[10px] lg:text-xs">Explore</span>
+            <span className="text-yellow-500 italic text-[10px]  lg:text-xs">Plus</span>
+            <img src="./fplus.svg" className="w-1 md:w-2" />
           </div>
         </Link>
       </div>
-      <div className="relative flex justify-end items-center px-2 lg:px-1 w-screen lg:w-1/2 lg:border rounded-md   lg:bg-[#f0f5ff]">       
+      <div className="lg:w-1/2">       
         <SearchContainer/>
       </div>
       <div className="menus flex w-[50%] md:w-1/3 ">
--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import useSuggestions from "../hooks/useSuggestions";
 
 const SearchContainer = () => {
-  const [searchText, setSearchText] = useState();
-  const [searchBox, setSearchBox] = useState(false);
+  const [searchText, setSearchText] = useState("trending");
+  const [searchBox, setSearchBox] = useState(true);
   const suggestionData = useSuggestions(searchText);
   const openSearchBox = () => {
     if (searchBox.length < 1) {
@@ -14,23 +14,23 @@ const SearchContainer = () => {
   const closeSearchBox = () => setSearchBox(false);
 
   return (
-    <>
-      <label className="">
+    <div className="md:relative border fixed w-full bg-white top-[59px] md:top-0 left-0  flex  items-center lg:px-2 lg:px-1   lg:border rounded-md   lg:bg-[#f0f5ff]">
+      <label className="px-1">
         <img src="/search.svg" className="w-6 lg:w-8 " 
         onClick={()=>setSearchBox(!searchBox)}
         />
       </label>
       <input
         type="text"
-        placeholder="hidden lg:inline Search for Products,Brands and More"
-        className="hidden lg:inline-block w-full p-2  lg:bg-[#f0f5ff] focus:border-transparent outline-none"
+        placeholder="Search for Products,Brands and More"
+        className="w-full h-fit p-2  lg:bg-[#f0f5ff] focus:border-transparent outline-none"
         value={searchText}
         onChange={(e) => setSearchText(e.target.value)}
         onFocus={openSearchBox}
         onBlur={closeSearchBox}
       />
       {searchBox && (
-        <div className="absolute w-full top-[42px] min-h-40  rounded-md bg-white p-5">
+        <div className="absolute w-full top-[41px] min-h-40  rounded-md bg-white ">
           {suggestionData?.suggestions?.map((suggestion) => (
             <div className="w-full flex gap-2 items-center p-2 cursor-pointer hover:bg-gray-100 rounded-md">
                 <img src="/search.svg" className="w-6"/>
@@ -39,7 +39,7 @@ const SearchContainer = () => {
           ))}
         </div>
       )}
-    </>
+    </div>
   );
 };
 
